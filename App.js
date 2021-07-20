b243e98d26b372ad25d2016726ea80cb4aef0876
bob <ahmedzaidmohd.com>
2021-07-20 17:26:51 +0530

    first commit

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, LogBox } from 'react-native';
 
 import HomeScreen from './src/HomeScreen'
+
+LogBox.ignoreAllLogs(true)
+
 export default function App() {
   return (
     <>
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,83 +1,207 @@
-import React, {useState, useEffect, Component} from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, SafeAreaView, TextInput} from 'react-native';
+import React, {useState} from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput, 
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 import axios from 'axios';
+// importing library to use Stopwatch and Timer
+import {Stopwatch, Timer} from 'react-native-stopwatch-timer';
 
-const HomeScreen = () => {
-    
-    const [id, setId] = useState('')
-    const [itemId, setItemId] = useState('')
-    const [itemTitle, setItemTitle] = useState('')
-    const [value, setValue] = useState('')
+const App = () => {
+ 
+  const [id, setId] = useState('')
+  const [itemId, setItemId] = useState('')
+  const [itemTitle, setItemTitle] = useState('')
+  const [value, setValue] = useState('')
+  const [time, setTime] = useState('')
+  const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+  const [resetStopwatch, setResetStopwatch] = useState(false);
+  const [loading, setLoading] = useState(false)
 
 
-    const fetchData = () => {
-        console.log("id:  " + id);
+  const fetchData = () => {
+
+      if(id !== ''){
+        resetTimer()
 
         axios.get('https://c965ab309344.ngrok.io/api/pdfs/'+id)
         .then(response => {
-            console.log(response.data[0])
+            console.log(response.data)
+            
+            startTimer()
             setItemId(response.data[0].id)
             setItemTitle(response.data[0].title)
+            setIsStopwatchStart(true);
+            setResetStopwatch(false);
+            setLoading(false)
         })
         .catch(error => {
             //console.error('There was an error!', error);
+            alert('Error: ' + error)
+            setTimeout(() => {
+                setLoading(false)
+            }, 500);
+        });
+      }else{
+        console.log('sd');    
+        setTimeout(() => {
+            setLoading(false)
+        }, 500);
             alert('Please check the Id you entered!')
-        });
-    }
+          
+      }
 
-    const updateData = () => {
+       
+  }
+
+  const updateData = () => {
       
-        const data = { 
-            title: 'First File' 
-        };
+      const formData = new FormData();
+      formData.append('file', time);
 
-        axios.put('https://c965ab309344.ngrok.io/api/pdfs/1', data)
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => {
-            
-            console.error('There was an error!', error);
-        });
-    }
-
-    useEffect(() => {
-        console.log("useEffect");
-        updateData()
-    }, [])
+      axios.post('https://c965ab309344.ngrok.io/api/pdfs/'+id,formData)
+      .then(response => {
+          console.log(response.data)
+          
+      })
+      .catch(error => {
+          console.error('There was an error!', error);
+          //alert('Please check the Id you entered!')
+      });
+  }
 
 
-    return(
-        <SafeAreaView style={{alignItems: 'center', flex:1}} >
-            <View >
-                <View style={{height: 100, marginTop: 100, justifyContent: 'center', alignItems: 'center'}} >
-                    <Text>ID:{itemId}</Text>
-                    <Text>Title: {itemTitle}</Text>
-                
-                    <TextInput
+  const resetTimer = () => {
+      console.log('restart');
+    setIsStopwatchStart(false);
+    setResetStopwatch(true);
+  } 
+
+
+  const startTimer = () => {
+    console.log('start');
+    setIsStopwatchStart(!isStopwatchStart);
+    setResetStopwatch(false);
+  } 
+
+  const setTimerValue = (timeVal) => {
+     // console.log('jkhgh ' + timeVal);
+      setTime(timeVal)
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <View>
+            <View style={styles.mainContainer} >
+                <View style={styles.textContainer} >
+                    {itemId === ''
+                    ?
+                    null
+                    :
+                        <View style={styles.textStyle} >
+                            <Text style={styles.text} >Id: {itemId}</Text>
+                            <Text style={styles.text} >Title: {itemTitle}</Text>
+                        </View>
+                    }
+                </View>
+                <TextInput
                     placeholder=" Enter the Item Id"
                     keyboardType="numeric"
-                    
+                    textAlign="center"
                     value={value}
                     onChangeText={(val) => {
                         setValue(val)
                         setId(val)
                     }}
-                    style={{height: 50, width: 200, borderWidth: 2, borderRadius: 10, fontSize: 24}}
-
+                    style={styles.textInputStyle}
                 />
-                </View>
-
-                
-
-                <TouchableOpacity 
-                    onPress={fetchData}
-                    style={{height: 50, width: 200, backgroundColor: 'red', borderRadius: 20, marginTop: 200, justifyContent: 'center', alignItems: 'center'}} >
-                    <Text>Button</Text>
-                </TouchableOpacity>
             </View>
-    </SafeAreaView>
-    )
-}
 
-export default HomeScreen
+            {loading 
+            ?<View 
+                style={styles.buttonContainer} >
+                <ActivityIndicator size="large" color="white" />
+                <Text style={styles.buttonText} >  Loading . . .</Text>
+            </View>
+            :
+            <TouchableOpacity 
+                onPress={() => {
+                    fetchData(); 
+                    updateData();
+                    setLoading(true)
+                }}
+                style={styles.buttonContainer} >
+                <Text style={styles.buttonText} >Button</Text>
+            </TouchableOpacity>
+            }
+
+            <Stopwatch
+                options={{text: {color: 'white'}}}
+                laps
+                msecs
+                start={isStopwatchStart}
+                reset={resetStopwatch}
+                getTime={(time) => {
+                setTimerValue(time)
+                }}
+            />
+        </View>
+      </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    mainContainer: {
+        height: 100, 
+        marginTop: 100, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    },
+    textContainer: {
+        margin: 20
+    },
+    text: {
+        fontSize: 22
+    },
+    textInputStyle: {
+        height: 50, 
+        width: 200, 
+        borderWidth: 2, 
+        borderRadius: 10, 
+        fontSize: 24
+    },
+    buttonContainer: {
+        height: 50,
+        width: 200, 
+        backgroundColor: '#3498DB', 
+        borderRadius: 20, 
+        marginTop: 200, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    buttonText: {
+        color: 'white', fontSize: 22
+    },
+    textStyle: {
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        width: 100
+    }
+});
+
